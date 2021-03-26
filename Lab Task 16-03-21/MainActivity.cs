f3ab380f31550d42096b895951df607e8d28a7f4
Md. Sadman Rahman <sadmanrman@gmail.com>
2021-03-26 21:29:32 +0600

    Moved all final term codes to this new repository

---
+++ Lab Task 16-03-21/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Task_16_03_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new SpecialSavings("Sadman Rahman", "789463", 50000);
+            Account a2 = new Overdraft("Ezaz Tehasy", "789465", 60000);
+
+            Person p = new Person("Sadman Rahman");
+
+             p.AddAccount(a1, a2);
+             
+
+             var acc = p.GetAccount("789463");
+
+             a1.Withdraw(30000);
+             a2.Withdraw(63000);
+             p.ShowAccontDetails();
+            
+        }
+    }
+}
---
+++ Lab Task 21-03-21 [Account]/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Task_21_03_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new Account("Sadman Rahman", "22345", 500);
+            Account a2 = new Account("Sakib Mahmud", "22346", 250);
+            Transaction t1 = new Transaction(a1,a2,200, "Credited");
+            
+            a1.Withdraw(200);
+            a1.Transfer(a2,100);
+            a1.Deposit(150);
+            a1.ShowAllInfo();
+        }
+    }
+}
